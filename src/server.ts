5122af1df702fe1c5a1f91f4ab61b1c2b4b60136
ipsita4 <i.bhatya@gmail.com>
2025-09-19 16:58:34 +0530

    cors fix 2

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // -------------------- Enable CORS --------------------
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
